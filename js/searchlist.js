b7a1eed7ced3bbf9819b4bcc1f14246d4cc04b00
jianhuatu <tujianhua123@gmail.com>
2015-10-26 17:09:21 +0800

    列表页面修改
    列表页面修改

--- js/searchlist.js
+++ js/searchlist.js
@@ -1,10 +1,13 @@
 $(function(){
 	this.searchData = new Object();
 
+	this.searchPage = 1;
+
 	this.createDomObj = function(){
 		this.conditionSelectObj = $("#conditionSelectObj");
 		this.searchNumObj = $("#searchNumObj");
 		this.conditionObj = $("#conditionObj");
+		this.searchListObj = $("#searchListObj");
 	}
 
 	this.createSearchData = function(){
@@ -21,7 +24,7 @@ $(function(){
 	this.createCondSelectHtml = function(){
 		var html = [];
 		for(var i=0,ilen=this.searchData['con'].length;i<ilen;i++){
-			html.push('<a href="javascript:void(0);" class="keyword">'+decodeURIComponent(this.searchData['con'][i])+'<em class="close">X</em></a>');
+			html.push('<a href="javascript:void(0);" class="keyword">'+decodeURIComponent(decodeURI(this.searchData['con'][i]))+'<em class="close">X</em></a>');
 		}
 		this.conditionSelectObj.html(html.join(""));
 	}
@@ -30,14 +33,16 @@ $(function(){
 		var self = this;
 		$.ajax({
 			url : "/xiaoqiang/php/searchlist.php",
-			data : "type="+this.searchData['type'].join(",")+"&con="+this.searchData['con'].join(",")+"&searchTime="+this.searchData['searchTime'],
+			data : "type="+this.searchData['type'].join(",")+"&con="+this.searchData['con'].join(",")+"&page="+this.searchPage+"&searchTime="+this.searchData['searchTime'],
 			dataType : "json",
 			type : "post",
 			success : function(data){
 				if(data.code!==0)return false;
 				if(data.searchTime!=self.searchData['searchTime'])return false;
 				self.createSearchCondHtml(data['info']['cond']);
-				self.createSearchListHtml(data['info']['list']);
+				self.createSeatchCountHtml(data['info']['count']);
+				if(data['info']['searchList']['page'] != self.searchPage)return false;
+				self.createSearchListHtml(data['info']['searchList']['list']);
 			}
 		})
 	}
@@ -48,7 +53,6 @@ $(function(){
 	}
 
 	this.createFloatCondHastitleHtml = function(data){
-		console.log(data);
 		var html = ['<div class="column"><dl><dt><strong>'+data['title']+'</strong></dt>'];
 
 		for(var i=0,ilen=data['child'].length;i<ilen;i++){
@@ -65,7 +69,7 @@ $(function(){
 	}
 
 	this.createFloatCondLayerHtml = function(data){
-		var html = ['<div class="down-layer" id="'+data['key']+'LayerObj" style="min-height:567px;display:none;">'];
+		var html = ['<div class="down-layer" id="'+data['key']+'LayerObj" style="display:none;">'];
 		for(var i=0,ilen=data['child'].length;i<ilen;i++){
 			if(data['child'][i]['title']){
 				html.push(this.createFloatCondHastitleHtml(data['child'][i]));
@@ -74,6 +78,20 @@ $(function(){
 			}
 		}
 		this.conditionObj.after(html.join(""));
+	}
+
+	this.setFloatCondLayerHeight = function(data){
+		var titleHeight = this.conditionObj.height();
+		for(var i=0,ilen=data.length;i<ilen;i++){
+			var thisObj = $("#"+data[i]+"LayerObj");
+			thisHeight = thisObj.height();
+			if(thisHeight < titleHeight){
+				thisObj.css({
+					"height" : titleHeight-20,
+					"min-height" : titleHeight-20
+				});
+			}
+		}
 	}
 
 	this.createSelectCondHtml = function(data){
@@ -89,18 +107,40 @@ $(function(){
 
 	this.createSearchCondHtml = function(data){
 		var html = new Array();
+		var floatKey = new Array();
 		for(var i=0,ilen=data.length;i<ilen;i++){
 			switch (data[i]['type']){
-				case "float" : html.push(this.createFloatCondHtml(data[i]));this.createFloatCondLayerHtml(data[i]);break;
+				case "float" : floatKey.push(data[i]['key']);html.push(this.createFloatCondHtml(data[i]));this.createFloatCondLayerHtml(data[i]);break;
 				case "select" : html.push(this.createSelectCondHtml(data[i]));break;
 			}
 		}
 
 		this.conditionObj.html(html.join(""));
+		this.setFloatCondLayerHeight(floatKey);
+	}
+
+	this.createSeatchCountHtml = function(data){
+		this.searchNumObj.html(data);
+	}
+
+	this.createSearchListLabelHtml = function(data){
+		if(!(data && data.length))return "";
+		var html = ['<div class="tags-wrap">'];
+		for(var i=0,ilen=data.length;i<ilen;i++){
+			html.push('<span class="tags">'+data[i]+'</span>');
+		}
+
+		html.push("</div>");
+		return html.join("");
 	}
 
 	this.createSearchListHtml = function(data){
+		var html = new Array();
+		for(var i=0,ilen=data.length;i<ilen;i++){
+			html.push('<div class="item-info'+(i%2 ? " hover" : "")+'">'+this.createSearchListLabelHtml(data[i]['label'])+'<h3><a href="javascript:void(0);" class="fontblue">'+data[i]['title']+'</a></h3><p class="text">'+data[i]['content']+'...</p></div>');
+		}
 
+		this.searchListObj.append(html);
 	}
 
 	this.createEvent = function(){
